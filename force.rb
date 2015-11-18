0a9823c16b30c6ebac0b3d94e5219108151572ce
Ethan Adams <ethanadamsm@s.dcsdk12.org>
2015-11-18 11:05:29 -0700

    Add calculate_forces method

---
+++ force.rb
@@ -0,0 +1,40 @@
+class Force
+
+	def initialize(x1, y1, x2, y2, mass1, mass2)
+		@x1 = x1
+		@y1 = y1 
+		@x2 = x2
+		@y2 = y2
+		@mass1 = mass1
+		@mass2 = mass2
+	end
+
+	def calculate_forces
+		[force_x, force_y]
+	end
+
+	def calculate_force
+		r = (((@x2 - @x1).to_i^2) + ((@y2 - @y1).to_i^2))
+		force_f = ((1/6.67408e11) * ((@mass1) * (@mass2))) / r
+		force_f
+	end
+
+	def force_x
+		if @x2 == @x1
+			return calculate_force
+		end
+		theta = Math.atan((@y2 - @y1)/(@x2 - @x1))
+		puts (@y2 - @y1)/(@x2 - @x1)
+		puts theta
+		forcex = calculate_force * Math.cos(theta)
+	end
+
+	def force_y
+		if @y2 == @y1
+			return calculate_force
+		end
+		theta = Math.atan((@y2 - @y1)/(@x2 - @y2))
+		forcex = calculate_force * Math.sin(theta)
+	end
+
+end
