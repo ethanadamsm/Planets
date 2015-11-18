651836a79e14b364b3a3f976e0e4bcec14616f39
Ethan Adams <ethanadamsm@s.dcsdk12.org>
2015-11-18 10:28:04 -0700

    Main, planets init and take from file

---
+++ main.rb
@@ -0,0 +1,34 @@
+require "gosu"
+require_relative "planet"
+
+class Window < Gosu::Window
+
+	def initialize(w)
+		w = (w.to_f)
+		w = w.to_i
+		super(500, 500)
+		file = File.open("simulations/planets.txt").read
+		array = file.split("\n")
+		scale = array[1].to_f
+		array_f = []
+		@planets = []
+		(2...array.length).each do |n|
+			array_f.push(array[n].split(" "))
+		end
+		array_f.each do |planet|
+			@planets.push(Planet.new(planet[0], planet[1], planet[2], planet[3], planet[4], planet[5], scale))
+		end
+		@background = Gosu::Image.new("images/starmap.jpg")
+	end
+
+	def draw
+		@planets.each do |planet|
+			planet.draw
+		end
+		@background.draw(0, 0, 0)
+	end
+
+end
+
+window = Window.new(2.50e2)
+window.show
